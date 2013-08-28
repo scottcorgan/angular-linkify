020fce8fbe29370dea21beaf0da8514a4d91e2c6
Scott Corgan <scottcorgan@gmail.com>
2013-08-27 20:07:39 -0700

    added directive support and optional parsing.
    Updated docs as well

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # angular-linkify
 
-Angular filter to linkify urls, "@" usernames, and hashtags.
+Angular filter and directive to linkify text. As of **v0.2.1**, angular-linkify works for twitter mentions, hashtags, and basic urls. Github support is COMING SOON!
 
 ## Install
 
@@ -16,16 +16,26 @@ Inject module into your application
 angular.module('YourApp', ['linkify']);
 ```
 
-User as a normal [AngularJS filter](http://docs.angularjs.org/guide/dev_guide.templates.filters.using_filters)
+User as a normal [AngularJS Filter](http://docs.angularjs.org/guide/dev_guide.templates.filters.using_filters) or [AngularJS Directive](http://docs.angularjs.org/guide/directive)
 
 ```html
+<!-- As a filter -->
 <div ng-controller="SomeCtrl">
   <div ng-bind-html-unsave="someValue | linkify"></div> <!-- linkified!! -->
 </div>
+
+<!-- As a filter, with twitter parsing -->
+<div ng-controller="SomeCtrl">
+  <div ng-bind-html-unsave="someValue | linkify:'twitter'"></div> <!-- linkified!! -->
+</div>
+
+<!-- As a directive, no twitter -->
+<div ng-bind="someModel" linkify></div>
+
+<!-- As a directive, with twitter parsing -->
+<div ng-bind="someModel" linkify="twitter"></div>
 ```
 
 ## Todo
 
-* Custom href attributes for linkified strings
-* Options to exclude "@"ifies and hastags
-* Make as injectable service and/or directive
+* add Github support
--- angular-linkify.js
+++ angular-linkify.js
@@ -1,26 +1,49 @@
 angular.module('linkify', []);
 
-angular.module('linkify').filter('linkify', function () {
-    'use strict';
-    
-    function linkify (_str) {
-        var tweet = _str.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) { 
-            var wrap = document.createElement('div');
-            var anch = document.createElement('a');
-            anch.href = url;
-            anch.target = "_blank";
-            anch.innerHTML = url;
-            wrap.appendChild(anch);
-            return wrap.innerHTML;
-        });
+angular.module('linkify')
+  .filter('linkify', function () {
+      'use strict';
+      
+      function linkify (_str, type) {
+          var tweet = _str.replace( /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+(?![^\s]*?")([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/ig, function(url) { 
+              var wrap = document.createElement('div');
+              var anch = document.createElement('a');
+              anch.href = url;
+              anch.target = "_blank";
+              anch.innerHTML = url;
+              wrap.appendChild(anch);
+              return wrap.innerHTML;
+          });
+          
+          // Twitter
+          if (type === 'twitter') {
+            tweet = tweet.replace(/(|\s)*@(\w+)/g, '$1<a href="https://www.twitter.com/$2" target="_blank">@$2</a>');
+            tweet = tweet.replace(/(^|\s)*#(\w+)/g, '$1<a href="https://twitter.com/search?q=%23$2" target="_blank">#$2</a>');
+          }
+          
+          // Github
+          if (type === 'github') {
+            
+          }
+          
+          return tweet;
+       }
+      
+      //
+      return function (text, type) {
+          return linkify(text, type);
+      };
+  })
+  .directive('linkify', function ($filter, $timeout, $parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var type = attrs.linkify;
         
-        tweet = tweet.replace(/(^|\s)@(\w+)/g, '$1<a href="http://www.twitter.com/$2" target="_blank">@$2</a>');
-        return tweet.replace(/(^|\s)#(\w+)/g, '$1<a href="http://search.twitter.com/search?q=%23$2" target="_blank">#$2</a>');
-     }
-    
-    //
-    return function (text) {
-        return linkify(text);
+        $timeout(function () {
+          element.html($filter('linkify')(element.html(), type));
+        });
+      }
     };
-});
+  });
 
